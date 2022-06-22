5f340d20b0a3a1b44e8ca03c64d79e4ff00c4822
Gustavo Cabral Ishizava <gustavo@MacBook-Pro-de-Gustavo.local>
2022-06-22 20:51:09 -0300

    add bulk operation model

--- src/MeuBolsoDigital.MongoDB.Context.UnitTests/Context/ChangeTracking/ChangeTrackerTests.cs
+++ src/MeuBolsoDigital.MongoDB.Context.UnitTests/Context/ChangeTracking/ChangeTrackerTests.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using MeuBolsoDigital.MongoDB.Context.Context.ChangeTracking;
 using MeuBolsoDigital.MongoDB.Context.UnitTests.Context.Common;
 using Xunit;
---
+++ src/MeuBolsoDigital.MongoDB.Context/Context/Operations/BulkOperationModel.cs
@@ -0,0 +1,22 @@
+using MongoDB.Driver;
+
+namespace MeuBolsoDigital.MongoDB.Context.Context.Operations
+{
+    public class BulkOperationModel<TDocument> where TDocument : class
+    {
+        public FilterDefinition<TDocument> Filter { get; private set; }
+        public TDocument Document { get; private set; }
+
+        public BulkOperationModel(FilterDefinition<TDocument> filter, TDocument document)
+        {
+            if (filter is null)
+                throw new ArgumentNullException(nameof(filter), "Filter cannot be null.");
+
+            if (document is null)
+                throw new ArgumentNullException(nameof(document), "Document cannot be null.");
+
+            Filter = filter;
+            Document = document;
+        }
+    }
+}
