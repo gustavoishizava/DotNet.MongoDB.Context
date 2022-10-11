8fad63e8b00078716fa4b9caa6c217803fc04960
Gustavo Cabral Ishizava <gustavo@MacBook-Pro-de-Gustavo.local>
2022-10-10 21:37:03 -0300

    fix update range method

--- src/DotNet.MongoDB.Context/Context/DbSet.cs
+++ src/DotNet.MongoDB.Context/Context/DbSet.cs
@@ -49,7 +49,7 @@ namespace DotNet.MongoDB.Context.Context
         {
             bulkOperationModels.ForEach(x => DbContext.ChangeTracker.AddEntry(new(EntryState.Modified, x.Document)));
 
-            var listWrites = bulkOperationModels.Select(x => new UpdateOneModel<TDocument>(x.Filter, x.Document.ToBsonDocument()) { IsUpsert = true });
+            var listWrites = bulkOperationModels.Select(x => new UpdateOneModel<TDocument>(x.Filter, new BsonDocument { { "$set", x.Document.ToBsonDocument() } }) { IsUpsert = true });
             await Collection.BulkWriteAsync(DbContext.ClientSessionHandle, listWrites, new() { IsOrdered = true });
         }
 
